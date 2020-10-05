4216c29985a63657e3734930594ecfca962a3ead
Miroslava <miroslavaroshchina@gmail.com>
2020-10-05 23:29:57 +0300

    task 1

---
+++ task_01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, tape your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello,\n{name}");
+        }
+    }
+}
