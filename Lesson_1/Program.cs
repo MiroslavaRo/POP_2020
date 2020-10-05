7c6873a5edca72fe105c2bb630aa022a79b6cce4
Miroslava <miroslavaroshchina@gmail.com>
2020-10-05 23:20:27 +0300

    class work

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -6,7 +6,91 @@ namespace Lesson_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task_3_Dates();
+            /* Console.WriteLine("Your age now is...");
+             int old = int.Parse(Console.ReadLine());
+             Console.WriteLine("After 10 years you will be {0} aged", old + 10);*/
+        }
+        static void MeAfterTen()
+        {
+            Console.WriteLine("Please enter your age:");
+            string ageAsString = Console.ReadLine();
+            try
+            {
+                int ageAfterTen = int.Parse(ageAsString) + 10;
+                Console.WriteLine("Your age after 10years will be " + ageAfterTen + " years");
+                Console.WriteLine($"Your age after 10years will be {ageAfterTen} years");
+            }
+            catch
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+        static void MeAfterTen2()
+        {
+            Console.WriteLine("Please enter your age:");
+            string ageAsString = Console.ReadLine();
+
+            int.TryParse(ageAsString, out int ageAsInt);
+            int ageAfterTen = ageAsInt + 10;
+            //Console.WriteLine("Your age after 10years will be " + ageAfterTen + " years");
+            Console.WriteLine($"Your age after 10years will be {ageAfterTen} years");
+
+
+        }
+        static void Rectangel()
+        {
+            Console.WriteLine("Please enter side A: ");
+            int SideA = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter side B: ");
+            int SideB = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area = " + SideA * SideB);
+            Console.WriteLine("Perimeter = " + 2 * (SideA * SideB));
+            Console.ReadKey();
+        }
+        static void Triangle()
+        {
+
+            Console.WriteLine("Please enter side A: ");
+            double SideA = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter side B: ");
+            double SideB = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter side C: ");
+            double SideC = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter side High: ");
+            double High = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area = " + (SideA * High) / 2);
+            Console.WriteLine("Perimeter = " + (SideA + SideB + SideC));
+            Console.ReadKey();
+        }
+
+
+        static void Task_3_Dates()
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine("Current date: ");
+            Console.WriteLine(date.ToShortDateString());
+
+            Console.WriteLine("Current time: ");
+            Console.WriteLine(date.ToShortTimeString());
+
+            Console.WriteLine("Date after 10 years: ");
+            Console.WriteLine(date.AddYears(10).ToShortDateString());
+
+            Console.WriteLine("Time after 10 years: ");
+            Console.WriteLine(date.AddYears(10).ToShortTimeString());
+
+            Console.WriteLine("Please enter a period");
+            int period = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Date and time after {period} years: ");
+            Console.WriteLine(date.AddYears(period).ToShortDateString()+"\n"+ date.AddYears(period).ToShortTimeString());
+            Console.ReadKey();
         }
     }
 }
