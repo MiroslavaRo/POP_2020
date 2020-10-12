9734cf4287231ab32265ce8ea75a4e27c8eb745a
Miroslava <miroslavaroshchina@gmail.com>
2020-10-12 18:52:50 +0300

    task1

--- POP_Class_work_lesson_2/Program.cs
+++ POP_Class_work_lesson_2/Program.cs
@@ -6,7 +6,62 @@ namespace POP_Class_work_lesson_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            task01();
+            //task02();
+            //task03();
+            //task04();
+            //task05();
         }
+        static void task01()
+        {
+            Console.Write("Print side A of the triangle: ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Print side B of the triangle: ");
+            int B = int.Parse(Console.ReadLine());
+            Console.Write("Print side C of the triangle: ");
+            int C = int.Parse(Console.ReadLine());
+                 
+                
+            if (A == B)
+            {
+                if (A == C)
+                {
+                    Console.WriteLine("Sides A, B and c are equal");
+                }
+                else
+                {
+                    Console.WriteLine("Sides A and B are equal");
+                }
+            }
+                       
+            else
+            {
+                if (C == B && C != A)
+                {
+                    Console.WriteLine("Sides C and B are equal");
+                }
+                else
+                {
+                    Console.WriteLine("There are no equal sides");
+                }
+            }
+        }
+        static void task02()
+        {
+
+        }
+        static void task03()
+        {
+
+        }
+        static void task04()
+        {
+
+        }
+        static void task05()
+        {
+
+        }
+
     }
 }
