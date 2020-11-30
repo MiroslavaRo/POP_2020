286398de50ecaa2d3f4f67844a9e1980080e0f12
Miroslava <miroslavaroshchina@gmail.com>
2020-11-30 18:54:51 +0200

    les10

---
+++ POP_Class_work_lesson_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POP_Class_work_lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ POP_Class_work_lesson_10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POP_Class_work_lesson_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
